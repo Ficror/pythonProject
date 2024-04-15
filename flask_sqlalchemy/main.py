198ed31947ecf4c4462445ac20254f602d595695
present_zak <egor.a.potapov@yandex.ru>
2024-04-15 18:41:42 +0300

    login

--- flask_sqlalchemy/main.py
+++ flask_sqlalchemy/main.py
@@ -2,10 +2,20 @@ from flask import Flask, render_template, redirect
 from data import db_session
 from data.users import User
 from data.catalog import Catalog
+from flask_sqlalchemy.forms.login import LoginForm
 from forms.user import RegisterForm
+from flask_login import LoginManager, login_user, login_required, logout_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
 def main():
@@ -17,7 +27,28 @@ def main():
 def index():
     db_sess = db_session.create_session()
     jobs = db_sess.query(Catalog).all()
-    return render_template("index.html")
+    return render_template("base.html", title="Start page")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -47,5 +78,4 @@ def reqister():
 
 
 if __name__ == '__main__':
-
     main()
