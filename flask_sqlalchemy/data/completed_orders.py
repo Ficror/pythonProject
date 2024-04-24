4d893d29b7ca071b67e6f9ec8275def7f5b200bd
Tim <timailiushin@yandex.ru>
2024-04-24 22:05:01 +0300

    end

--- flask_sqlalchemy/data/completed_orders.py
+++ flask_sqlalchemy/data/completed_orders.py
@@ -6,6 +6,6 @@ from sqlalchemy import orm
 class Completed_orders(SqlAlchemyBase):
     __tablename__ = "completed_orders"
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    username = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"), unique=True)
+    username = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
     list_of_product = sqlalchemy.Column(sqlalchemy.String)
     user = orm.relationship("User")
--- flask_sqlalchemy/main.py
+++ flask_sqlalchemy/main.py
@@ -1,7 +1,9 @@
+import flask_login
 from flask import Flask, render_template, redirect
 from data import db_session
 from data.users import User
 from data.catalog import Catalog
+from data.completed_orders import Completed_orders
 from flask_sqlalchemy.forms.login import LoginForm
 from forms.user import RegisterForm
 from flask_login import LoginManager, login_user, login_required, logout_user
@@ -26,8 +28,15 @@ def main():
 @app.route("/")
 def index():
     db_sess = db_session.create_session()
-    jobs = db_sess.query(Catalog).all()
-    return render_template("index.html", title="Start page")
+    if flask_login.current_user.is_authenticated:
+        if flask_login.current_user.email == "admin@gmail.com":
+            orders = db_sess.query(Completed_orders).all()
+            user_id = db_sess.query(User).all()
+            if orders == []:
+                return render_template("empty_orders.html", flag=True)
+            return render_template("admin_page.html", compl_orders=orders, adress=user_id, flag=True)
+        return render_template("base.html", title="Start page")
+    return render_template("base.html", title="Start page")
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -88,10 +97,71 @@ def products(product_name):
 @app.route('/in_basket/<product>')
 def in_basket(product):
     db_sess = db_session.create_session()
-    name = db_sess.query(Catalog).filter(Catalog.id == product).first()
+    name = db_sess.query(Catalog).filter(Catalog.id == int(product)).first()
     name.in_basket = 1
     db_sess.commit()
-    return redirect('/in_basket/<product>')
+    return redirect("/" + name.type)
+
+
+@app.route('/basket')
+def basket():
+    db_sess = db_session.create_session()
+    summ = 0
+    names = db_sess.query(Catalog).filter(Catalog.in_basket == 1).all()
+    for i in names:
+        summ += int(i.price)
+    if names == []:
+        return render_template("empty_basket.html")
+    else:
+        return render_template("basket.html", names=names, summ=summ)
+
+
+@app.route('/not_in_basket/<product>')
+def not_in_basket(product):
+    db_sess = db_session.create_session()
+    name = db_sess.query(Catalog).filter(Catalog.id == int(product)).first()
+    name.in_basket = 0
+    db_sess.commit()
+    return redirect("/basket")
+
+
+@app.route('/order', methods=['GET', 'POST'])
+def order():
+    return render_template("basket_end.html", message="Вы не зарегистрированны")
+
+
+@app.route('/order_good', methods=['GET', 'POST'])
+def order_good():
+    db_sess = db_session.create_session()
+    names_name = []
+    names = db_sess.query(Catalog).filter(Catalog.in_basket == 1).all()
+    for i in names:
+        names_name.append(i.name)
+    names_name = ", ".join(names_name)
+    for i in names:
+        i.in_basket = 0
+    db_sess.commit()
+    user_id = flask_login.current_user.id
+    compl_ord = Completed_orders(
+        username=user_id,
+        list_of_product=names_name
+    )
+    db_sess.add(compl_ord)
+    db_sess.commit()
+    return render_template("basket_end.html", message1="Заказ успешно оформлен",
+                           message2="С вами свяжутся для уточнения деталей заказа", flag=False)
+
+
+@app.route('/news_delete/<int:id>', methods=['GET', 'POST'])
+@login_required
+def news_delete(id):
+    db_sess = db_session.create_session()
+    compl_ord = db_sess.query(Completed_orders).filter(Completed_orders.id == id).first()
+    if compl_ord:
+        db_sess.delete(compl_ord)
+        db_sess.commit()
+    return redirect('/')
+
 
 if __name__ == '__main__':
     main()
